f4d1eca9dfbfe62976652a4302898477e0475b4a
Willy Brauner <willybrauner@gmail.com>
2023-09-18 10:17:51 +0200

    Update react example

---
+++ examples/react-nested-routes/src/components/app/App.tsx
@@ -0,0 +1,56 @@
+import "./App.css"
+import { useEffect, useRef, useState } from "react"
+import { historyPlugin, Router } from "@wbe/low-router"
+import { Home } from "../../pages/Home.tsx"
+import { About } from "../../pages/About.tsx"
+
+function App() {
+  const router = useRef<Router>(null)
+  const routes = [
+    {
+      path: "/",
+      name: "home",
+      action: () => <div>HOme</div>,
+    },
+    {
+      path: "/about",
+      name: "about",
+      action: About,
+    },
+  ]
+
+  // store the component to render
+  const [component, setComponent] = useState(null)
+
+  useEffect(() => {
+    router.current = new Router(routes, {
+      debug: true,
+      plugins: [historyPlugin],
+      onResolve: (context, actionResult) => {
+        setComponent(actionResult)
+      },
+    })
+  }, [])
+
+  return (
+    <div>
+      {/* NAV */}
+      <nav>
+        {routes.map((route, index) => (
+          <button
+            key={index}
+            children={route.name}
+            onClick={() => {
+              router.current?.resolve(route.path)
+            }}
+          />
+        ))}
+      </nav>
+
+      {/* RENDER */}
+      <div className={"page"}>{component}</div>
+    </div>
+  )
+}
+
+export default App
