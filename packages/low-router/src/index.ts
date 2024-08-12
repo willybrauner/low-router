71279f38d1ac715e94c06476e7368cb28823a2dd
Willy Brauner <willybrauner@gmail.com>
2024-08-13 00:21:19 +0200

    feat: extract compile path (#34)

--- packages/low-router/src/index.ts
+++ packages/low-router/src/index.ts
@@ -1,6 +1,6 @@
 export { LowRouter } from "./LowRouter"
 export type { RouteContext, Route, RouteParams, RouteProps, RouterOptions } from "./types"
-export { createMatcher, pathToRegexp } from "./createMatcher"
+export { createMatcher, pathToRegexp } from "./utils/createMatcher"
 
 // additional exports
 export { createBrowserHistory } from "./utils/browserHistory"
--- packages/low-router/tests/compilePath.test.ts
+++ packages/low-router/tests/compilePath.test.ts
@@ -1,20 +1,21 @@
-import { describe, expect, it, vi } from "vitest"
-import { LowRouter } from "../src"
+import { describe, expect, it } from "vitest"
+import { compilePath } from "../src/utils/compilePath"
 
 describe.concurrent("compile path", () => {
   it("should compile path with params", () => {
-    expect(LowRouter.compilePath("/foo/:id?/zoo")({ id: "bar" })).toBe("/foo/bar/zoo");
-    expect(LowRouter.compilePath("/foo/:id?/zoo")({ })).toBe("/foo/zoo");
-    expect(LowRouter.compilePath("/foo/:id?/zoo")({ })).toBe("/foo/zoo");
+    expect(compilePath("/foo/:id?/zoo")({ id: "bar" })).toBe("/foo/bar/zoo")
+    expect(compilePath("/foo/:id?/zoo")({})).toBe("/foo/zoo")
+    expect(compilePath("/foo/:id?/zoo")({})).toBe("/foo/zoo")
 
-    expect(LowRouter.compilePath("/foo/zoo?a=b")({ })).toBe("/foo/zoo?a=b");
-    expect(LowRouter.compilePath("/:id/zoo?a=b")({ id: "bar" })).toBe("/bar/zoo?a=b");
-    expect(LowRouter.compilePath("/:id?/zoo?a=b")({ id: "bar" })).toBe("/bar/zoo?a=b");
-    expect(LowRouter.compilePath("/zoo/:bar?")({ bar: "zoo" })).toBe("/zoo/zoo");
-    expect(LowRouter.compilePath("/zoo/:bar?params=one")({ bar: "zoo" })).toBe("/zoo/zoo?params=one");
+    expect(compilePath("/foo/zoo?a=b")({})).toBe("/foo/zoo?a=b")
+    expect(compilePath("/:id/zoo?a=b")({ id: "bar" })).toBe("/bar/zoo?a=b")
+    expect(compilePath("/:id?/zoo?a=b")({ id: "bar" })).toBe("/bar/zoo?a=b")
+    expect(compilePath("/zoo/:bar?")({ bar: "zoo" })).toBe("/zoo/zoo")
+    expect(compilePath("/zoo/:bar?params=one")({ bar: "zoo" })).toBe("/zoo/zoo?params=one")
 
-    expect(LowRouter.compilePath("/test/:id#foobar")({id: "foo"})).toBe("/test/foo#foobar")
-    expect(LowRouter.compilePath("/test/:params?lang=en")({params: "helloParams"})).toBe("/test/helloParams?lang=en")
-
+    expect(compilePath("/test/:id#foobar")({ id: "foo" })).toBe("/test/foo#foobar")
+    expect(compilePath("/test/:params?lang=en")({ params: "helloParams" })).toBe(
+      "/test/helloParams?lang=en"
+    )
   })
 })
