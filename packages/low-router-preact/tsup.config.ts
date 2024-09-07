01366c0a0679759b9b12ec1e69b638a9b72211de
Willy Brauner <willybrauner@gmail.com>
2024-09-07 22:20:41 +0200

    feat: internalize deps

--- packages/low-router-preact/tsup.config.ts
+++ packages/low-router-preact/tsup.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from "tsup"
-// @ts-ignore
-import fs from "node:fs/promises"
+// import fs from "node:fs/promises"
 
 export default defineConfig([
-  {
-    entry: {
-      "preact-deps": "src/preact-deps.ts",
-    },
-    splitting: true,
-    clean: true,
-    dts: true,
-    format: ["esm"],
-    minify: true,
-    external: ["preact", "preact/hooks", "preact/compat"],
-  },
+  // {
+  //   entry: {
+  //     "preact-deps": "src/preact-deps.ts",
+  //   },
+  //   splitting: true,
+  //   clean: true,
+  //   dts: true,
+  //   format: ["esm"],
+  //   minify: true,
+  //   external: ["preact", "preact/hooks", "preact/compat"],
+  // },
+
   {
     entry: {
       "low-router-preact": "src/index.ts",
@@ -24,28 +24,29 @@ export default defineConfig([
     format: ["esm"],
     minify: true,
     external: [
-      /preact-deps/,
       "preact",
+      "preact/compat",
       "preact/hooks",
       "@wbe/utils",
       "@wbe/debug",
       "@wbe/low-router",
+      // /preact-deps/,
     ],
 
-    esbuildPlugins: [
-      {
-        name: "replace-import",
-        setup(build) {
-          build.onLoad({ filter: /\.ts$/ }, async (args) => {
-            let contents = await fs.readFile(args.path, "utf8")
-            contents = contents.replace(/from\s+['"]\.\.\/preact-deps['"]/g, 'from "./preact-deps"')
-            return {
-              contents,
-              loader: "default",
-            }
-          })
-        },
-      },
-    ],
+    // esbuildPlugins: [
+    //   {
+    //     name: "replace-import",
+    //     setup(build) {
+    //       build.onLoad({ filter: /\.ts$/ }, async (args) => {
+    //         let contents = await fs.readFile(args.path, "utf8")
+    //         contents = contents.replace(/from\s+['"]\.\.\/preact-deps['"]/g, 'from "./preact-deps"')
+    //         return {
+    //           contents,
+    //           loader: "default",
+    //         }
+    //       })
+    //     },
+    //   },
+    // ],
   },
 ])
